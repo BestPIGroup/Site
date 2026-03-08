2f4029a1a3fd80a2ab39cf2f4f6e043af0223342
MatheusSouzaR <matheus.srodrigues@sptech.school>
2026-03-08 12:32:07 -0300

    Finalizando responsividade e apagando arquivos desnecessários

--- SITE/app.js
+++ SITE/app.js
@@ -19,10 +19,6 @@ var indexRouter = require("./src/routes/index");
 var usuarioRouter = require("./src/routes/usuarios");
 var unidadeRouter = require("./src/routes/unidade");
 var servidorRouter = require("./src/routes/servidores")
-var avisosRouter = require("./src/routes/avisos");
-var medidasRouter = require("./src/routes/medidas");
-//var aquariosRouter = require("./src/routes/aquarios");
-var empresasRouter = require("./src/routes/empresas");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -34,10 +30,6 @@ app.use("/", indexRouter);
 app.use("/usuarios", usuarioRouter);
 app.use("/unidade", unidadeRouter);
 app.use("/servidores", servidorRouter);
-app.use("/avisos", avisosRouter);
-app.use("/medidas", medidasRouter);
-//app.use("/aquarios", aquariosRouter);
-app.use("/empresas", empresasRouter);
 
 app.listen(PORTA_APP, function () {
     console.log(`
