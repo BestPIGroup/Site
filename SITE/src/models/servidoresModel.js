d6319240c2cecb19922f3b345d1a5aa5550bcdea
ak-miyuki <andreia.kubota@sptech.school>
2026-03-29 22:12:33 -0300

    novo usuario no .env.dev, campo senha tipo password, campo status do servidor no model status_servidor igual ao bd

--- SITE/src/models/servidoresModel.js
+++ SITE/src/models/servidoresModel.js
@@ -2,7 +2,7 @@ var database = require("../database/config");
 
 function cadastrar_servidor(fornecedor, modelo, numero, status, fk_unidade){
         console.log("ACESSEI O MODEL DA UNIDADE \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar_Unidade(): ")
-        var instrucaoSql = `INSERT INTO servidor(fornecedor, modelo, numero_serie, status, fk_unidade) VALUES('${fornecedor}', '${modelo}', '${numero}', '${status}', '${fk_unidade}');`;
+        var instrucaoSql = `INSERT INTO servidor(fornecedor, modelo, numero_serie, status_servidor, fk_unidade) VALUES('${fornecedor}', '${modelo}', '${numero}', '${status}', '${fk_unidade}');`;
         console.log("Executando a instrução SQL: \n" + instrucaoSql);
         return database.executar(instrucaoSql);
 }
