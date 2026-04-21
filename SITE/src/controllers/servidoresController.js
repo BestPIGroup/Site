a57724e1afe5e0e1e7d4a35ebf54d37d7c72e39d
murilo2305 <murilo.santos.silva.v@gmail.com>
2026-04-20 22:13:31 -0300

    alterações finais

--- SITE/src/controllers/servidoresController.js
+++ SITE/src/controllers/servidoresController.js
@@ -4,18 +4,14 @@ function cadastrar_servidor(req, res) {
 
     var Mac = req.body.MacServer;
     var modelo = req.body.modeloServer;
-    var numero = req.body.numeroServer;
-    var status = req.body.statusServer;
     var fk_unidade = req.body.fk_unidadeServer
 
     if (Mac == undefined) {
         res.status(400).send("Seu Mac está undefined!");
     } else if (modelo == undefined) {
         res.status(400).send("Seu modelo está undefined!");
-    } else if (numero == undefined) {
-        res.status(400).send("Seu numero undefined!");
     } else {
-        servidoresModel.cadastrar_servidor(Mac, modelo, numero, status, fk_unidade)
+        servidoresModel.cadastrar_servidor(Mac, modelo, fk_unidade)
             .then(
                 function (resultado) {
                     res.json(resultado);
