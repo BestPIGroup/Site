b9a6ba17d6aee38c82176e92b9047b894e55eedb
murilo2305 <murilo.santos.silva.v@gmail.com>
2026-04-21 11:13:40 -0300

    correção: tirando modelo adicionando status

--- SITE/src/controllers/servidoresController.js
+++ SITE/src/controllers/servidoresController.js
@@ -3,15 +3,15 @@ var servidoresModel = require("../models/servidoresModel");
 function cadastrar_servidor(req, res) {
 
     var Mac = req.body.MacServer;
-    var modelo = req.body.modeloServer;
-    var fk_unidade = req.body.fk_unidadeServer
+    var status = req.body.statusServer;
+    var fk_unidade = req.body.fk_unidadeServer;
 
     if (Mac == undefined) {
         res.status(400).send("Seu Mac está undefined!");
-    } else if (modelo == undefined) {
-        res.status(400).send("Seu modelo está undefined!");
+    } else if (status == undefined) {
+        res.status(400).send("Seu status está undefined!");
     } else {
-        servidoresModel.cadastrar_servidor(Mac, modelo, fk_unidade)
+        servidoresModel.cadastrar_servidor(Mac, status, fk_unidade)
             .then(
                 function (resultado) {
                     res.json(resultado);
